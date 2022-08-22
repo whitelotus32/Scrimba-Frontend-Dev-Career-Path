c82713ba52168045015274b1bc6bd002f6e56a85
Nicholas Dixon <naseer2152@gmail.com>
2022-08-22 14:20:25 -0700

    Added exercise challenge

--- Module 3: Making websites interactive/Build a passenger counter app/Reassigning and incrementing/index.js
+++ Module 3: Making websites interactive/Build a passenger counter app/Reassigning and incrementing/index.js
@@ -2,7 +2,11 @@
 
 let count = 5
 
-
+// count + 1
+count = count + 1 
 
 console.log(count)
 
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally increase it to 70
+// Console.log the value after each step
