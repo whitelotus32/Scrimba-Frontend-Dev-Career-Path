04dd43c70e4aab923bd20386ce257a429d6d6c06
Nicholas Dixon <naseer2152@gmail.com>
2022-08-22 09:31:28 -0700

    Created JS variable

--- Module 3: Making websites interactive/Write your first JS variable/index.js
+++ Module 3: Making websites interactive/Write your first JS variable/index.js
@@ -1,2 +1,11 @@
 // document.getElementById("count").innerText = 5
 
+let count = 0
+
+// 1. Create a variable, myAge, and set its value to your age
+
+let myAge = 32
+
+// 2. Log the myAge variable to the console
+
+console.log(myAge)
