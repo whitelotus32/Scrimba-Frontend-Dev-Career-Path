94ae35a3fcd8d56dcb8effa71d584aefc56bf95d
Nicholas Dixon <naseer2152@gmail.com>
2022-08-22 14:21:56 -0700

    Added solution

--- Module 3: Making websites interactive/Build a passenger counter app/Reassigning and incrementing/index.js
+++ Module 3: Making websites interactive/Build a passenger counter app/Reassigning and incrementing/index.js
@@ -10,3 +10,15 @@ console.log(count)
 // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
 // Decrease it down to 25, and then finally increase it to 70
 // Console.log the value after each step
+
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
