f189327b7643bbdf6d3e5e913a7f80b9f131d988
Nicholas Dixon <naseer2152@gmail.com>
2022-08-23 09:48:58 -0700

    Added increment function

--- Module 3: Making websites interactive/Build a passenger counter app/The onclick event listener/index.js
+++ Module 3: Making websites interactive/Build a passenger counter app/The onclick event listener/index.js
@@ -4,22 +4,11 @@
 // count = count + 1
 // console.log(count)
 
-// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
-// Decrease it down to 25, and then finally increase it to 70
-// Console.log the value after each step
+// initialize the count as 0 
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
 
-let bonusPoints = 50
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
-
-
-
-
+function increment() {
+    console.log("The button was clicked")
+}
