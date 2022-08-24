4212f69b1880fc5a06a66dd09d217c15c05c6c7b
Nicholas Dixon <naseer2152@gmail.com>
2022-08-24 05:56:03 -0700

    Logged 410 not 14

--- Module 3: Making websites interactive/Build a passenger counter app/Strings vs. Numbers/index.js
+++ Module 3: Making websites interactive/Build a passenger counter app/Strings vs. Numbers/index.js
@@ -1,5 +1,11 @@
-let name = "whitelotus32"
-let greeting = "Hi, my name is "
+let name = 42
+let greeting = "Hi, my name is 42"
 let myGreeting = greeting + name
 console.log(myGreeting)
 
+let points = 4
+let bonusPoints = "10"
+
+let totalPoints = points + bonusPoints
+
+console.log(totalPoints)
