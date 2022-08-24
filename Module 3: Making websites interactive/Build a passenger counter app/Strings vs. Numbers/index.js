18734033c18c94a1340346ca7d3759a6a618938d
Nicholas Dixon <naseer2152@gmail.com>
2022-08-24 05:58:55 -0700

    Added challenges

--- Module 3: Making websites interactive/Build a passenger counter app/Strings vs. Numbers/index.js
+++ Module 3: Making websites interactive/Build a passenger counter app/Strings vs. Numbers/index.js
@@ -3,9 +3,16 @@ let greeting = "Hi, my name is 42"
 let myGreeting = greeting + name
 console.log(myGreeting)
 
-let points = 4
-let bonusPoints = "10"
+// let points = 4
+// let bonusPoints = "10"
 
-let totalPoints = points + bonusPoints
+// let totalPoints = points + bonusPoints
 
-console.log(totalPoints)
+// console.log(totalPoints)
+
+//what will these return?
+
+console.log(4 + 5) 
+console.log("2" + "4")
+console.log("5" + 1)
+console.log(100 + "100")
