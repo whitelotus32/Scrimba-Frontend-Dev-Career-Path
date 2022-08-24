9b94c6527568975c8b8a667d18f157f5e0aba551
Nicholas Dixon <naseer2152@gmail.com>
2022-08-24 05:48:20 -0700

    Added solution to practice exercise

---
+++ Module 3: Making websites interactive/Build a passenger counter app/Write your first string variable/index.js
@@ -0,0 +1,12 @@
+let username = "whitelotus32"
+
+// Create a variable, message, that stores the string: "You have three new notifications"
+
+let message = "You have three new notifications"
+
+console.log(message + ", " + username + "!")
+
+
+// Create a variable, messageToUser, that contains the message we have logged
+
+let messageToUser = message + "," + username + "!"
