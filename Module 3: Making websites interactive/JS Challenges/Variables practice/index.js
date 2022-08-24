3c953cf98ac58e35c4d1f70b6b89dd6ceddbdfe3
Nicholas Dixon <naseer2152@gmail.com>
2022-08-24 12:27:06 -0700

    Added solution

--- Module 3: Making websites interactive/JS Challenges/Variables practice/index.js
+++ Module 3: Making websites interactive/JS Challenges/Variables practice/index.js
@@ -1,7 +1,10 @@
 // Create two variables, firstName and lastName
-
+let firstName = "Nick"
+let lastName = "Dixon"
 // Concatenate the two variables into a third variable called fullName
+
+let fullName = firstName + " " + lastName
 
 // Log fullName to the console
 
-
+console.log(fullName)
