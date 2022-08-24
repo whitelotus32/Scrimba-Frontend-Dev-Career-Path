0e317e572709b41fe5007f886f4c742c8ffd5072
Nicholas Dixon <naseer2152@gmail.com>
2022-08-24 09:25:02 -0700

    Added solution

--- Module 3: Making websites interactive/Build a passenger counter app/Improve the message wth string concatenation/index.js
+++ Module 3: Making websites interactive/Build a passenger counter app/Improve the message wth string concatenation/index.js
@@ -1,11 +1,14 @@
 let welcomeEl = document.getElementById("welcome-el")
 
-let name = "Per Harald Borgen"
+let name = "whitelotus32"
 let greeting = "Welcome back "
 
 welcomeEl.innerText = greeting + name
 
-// Add an emoji to the end!
+// Add an emoji to the end! 👋
 // WRITE YOUR CODE BELOW HERE
 // HINT: count = count + 1
 
+// welcomeEl.innerText = welcomeEl.innerText + "👋"
+welcomeEl.innerText += "👋"
+// the += operator takes the current value and adds the following after it
