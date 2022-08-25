91ca99bb3e25dfd386bc3dd1ac87d8de5f1d8853
Nicholas Dixon <naseer2152@gmail.com>
2022-08-25 09:37:23 -0700

    Added sloution

--- Module 3: Making websites interactive/JS Challenges/Rendering an error message/index.js
+++ Module 3: Making websites interactive/JS Challenges/Rendering an error message/index.js
@@ -2,4 +2,10 @@
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
 
+let errorParagraph = document.getElementById("error")
+console.log(errorParagraph)
 
+function purchase() {
+    console.log("button clicked")
+    errorParagraph.textContent = "Something went wrong, please try again"
+}
